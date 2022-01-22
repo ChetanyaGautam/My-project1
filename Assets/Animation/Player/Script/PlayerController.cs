64fdd6547235e53ba3f4d4b5ada99baee2711759
Chetanya Gautam <chetangautam37@gmail.com>
2022-01-22 17:02:03 +0530

    Chapter 2 - Assignment - Crouch

--- Assets/Animation/Player/Script/PlayerController.cs
+++ Assets/Animation/Player/Script/PlayerController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    bool Running = false;
+    bool Isjumping = false;
+
+
     private void Awake()
     {
         Debug.Log("Player Controller Awake");
@@ -13,20 +17,46 @@ public class PlayerController : MonoBehaviour
      {
          Debug.Log("Collision : " + collision.gameObject.name);
      }*/
+   
 
-    private void Update()
+    public void Update()
     {
+
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(speed));
 
         Vector3 scale = transform.localScale;
-        if(speed < 0)
+        if (speed < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
-        }else if(speed > 0)
+        }
+        else if (speed > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+
+
+        float jump = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Jump", Mathf.Abs(jump));
+
+        Vector3 localscale = transform.localScale;
+        if (jump < 0)
+        {
+            scale.y = -1f * Mathf.Abs(scale.y);
+        }
+
+        Running = Input.GetKey(KeyCode.LeftShift);
+        Isjumping = Input.GetKey(KeyCode.LeftControl);
+            
+
+            animator.SetBool("Bool", Running);
+            animator.SetBool("Bool2", Isjumping);
+           
+        
+
     }
+    
+
 }
