33ac8492fe59f82aef54901cbee010c8579bc396
Chetanya Gautam <chetangautam37@gmail.com>
2022-01-24 19:15:11 +0530

    Chapter 3 - Mini Assignment 1 - Movement
    i did it already walk and walking flipping animation please check

--- Assets/Animation/Player/Script/PlayerController.cs
+++ Assets/Animation/Player/Script/PlayerController.cs
@@ -6,57 +6,83 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     bool Running = false;
-    bool Isjumping = false;
+    bool Crouch = false;
+    public float speed;
+    public float jump;
+   
+    private Rigidbody2D Rgd2D;
 
 
     private void Awake()
     {
         Debug.Log("Player Controller Awake");
+        Rgd2D = gameObject.GetComponent<Rigidbody2D>();
+        SpriteRenderer sprites = gameObject.GetComponent<SpriteRenderer>();
     }
-    /* private void OnCollisionEnter2D(Collision2D collision)
-     {
-         Debug.Log("Collision : " + collision.gameObject.name);
-     }*/
-   
 
-    public void Update()
+    private void Update()
+    {
+        float speed1 = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
+        MoveCharacter(speed1, vertical);
+        PlayMovementAnimation(speed1, vertical);
+    }
+    private void MoveCharacter(float speed1 , float vertical) 
     {
 
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        /* move Horizontally*/
+        Vector3 position = transform.position;
+        position.x += speed1 * speed * Time.deltaTime;
+        transform.position = position;
+
+
+        if (vertical > 0)
+        {
+            Rgd2D.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
+        }
+
+
+
+
+
+    }
+
+
+    private void PlayMovementAnimation(float speed1, float vertical)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(speed1));
 
         Vector3 scale = transform.localScale;
-        if (speed < 0)
+        if (speed1 < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
-        }
-        else if (speed > 0)
+        } 
+        else if (speed1 > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
-        transform.localScale = scale;
+        transform.localScale = scale;  
+
+         /*jumpping*/
+        if (vertical > 0)
+        {
+            animator.SetBool ("Jump", true);
+           
+        }else
+        {
+            animator.SetBool("Jump", false);
+        }
+      
 
 
 
-        float jump = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Jump", Mathf.Abs(jump));
 
         Vector3 localscale = transform.localScale;
-        if (jump < 0)
-        {
-            scale.y = -1f * Mathf.Abs(scale.y);
-        }
-
-        Running = Input.GetKey(KeyCode.LeftShift);
-        Isjumping = Input.GetKey(KeyCode.LeftControl);
-            
-
-            animator.SetBool("Bool", Running);
-            animator.SetBool("Bool2", Isjumping);
-           
         
-
+       
+            Running = Input.GetKey(KeyCode.LeftShift);
+            Crouch = Input.GetKey(KeyCode.LeftControl);
+            animator.SetBool("Bool", Running);
+            animator.SetBool("Bool2", Crouch);
     }
-    
-
 }
